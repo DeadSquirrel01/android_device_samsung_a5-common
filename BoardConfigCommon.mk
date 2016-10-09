5bbaafe11607a117d73119915853bdaf57bbb37f
DeadSquirrel01 <m.marcaccini01@gmail.com>
2016-10-09 16:26:01 +0200

    We want enforcing!

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ TARGET_CPU_CORTEX_A53 := true
 # Kernel
 TARGET_KERNEL_ARCH := arm
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=enforcing
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
