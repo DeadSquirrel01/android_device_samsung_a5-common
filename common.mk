b0d1a41fc4199627a6e16238ed0d5e3bee192af3
DeadSquirrel01 <m.marcaccini01@gmail.com>
2017-02-25 11:49:40 +0100

    [DO NOT MERGE YET]Implement audio amplifier

--- common.mk
+++ common.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8916 \
+    audio_amplifier.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
