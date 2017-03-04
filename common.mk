9ced61e80722a4ad3256cb525f188478a185b5a0
DeadSquirrel01 <m.marcaccini01@gmail.com>
2017-03-04 23:51:45 +0000

    Fix LA.BR.1.2.9 wcnss caf update

--- common.mk
+++ common.mk
@@ -253,7 +253,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prima/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/prima/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
-    $(LOCAL_PATH)/prima/WCNSS_wlan_dictionary.dat:persist/WCNSS_wlan_dictionary.dat
 
 # Wi-Fi
 PRODUCT_COPY_FILES += \
