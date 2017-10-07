0d984c8f19ab8801c297a33db1b85b179dcbbea1
DeadSquirrel01 <m.marcaccini01@gmail.com>
2017-10-07 07:32:17 +0000

    Add modem type

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,7 @@ BLUETOOTH_HCI_USE_MCT                       := true
 # RIL
 BOARD_RIL_CLASS                  := ../../../device/samsung/a5-common/ril/
 BOARD_PROVIDES_LIBRIL            := true
+BOARD_MODEM_TYPE                 := xmm62xx
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT          := true
