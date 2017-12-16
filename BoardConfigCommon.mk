2c47180f0b8922a3a76636645bfea5c7199b20f2
DeadSquirrel01 <m.marcaccini01@gmail.com>
2017-12-16 10:09:58 +0000

    Use samsung mkbootimg to remove ANDROID IS NOT SEANDROID ENFORCING message
    A

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,8 @@ TARGET_CPU_CORTEX_A53   := true
 # Kernel
 TARGET_KERNEL_ARCH         := arm
 BOARD_DTBTOOL_ARGS         := -2
-BOARD_CUSTOM_BOOTIMG_MK    := $(DEVICE_PATH)/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG       := true
+BOARD_CUSTOM_BOOTIMG_MK    := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_CMDLINE       := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=enforcing
 BOARD_KERNEL_BASE          := 0x80000000
 BOARD_RAMDISK_OFFSET       := 0x02000000
