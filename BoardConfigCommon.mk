34974a6433b2072edd2ecea464a85ff3d90a0f9c
DeadSquirrel01 <m.marcaccini01@gmail.com>
2017-12-16 02:19:51 +0000

    Ready to enforcing!

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ TARGET_CPU_CORTEX_A53   := true
 TARGET_KERNEL_ARCH         := arm
 BOARD_DTBTOOL_ARGS         := -2
 BOARD_CUSTOM_BOOTIMG_MK    := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE       := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE       := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=enforcing
 BOARD_KERNEL_BASE          := 0x80000000
 BOARD_RAMDISK_OFFSET       := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET   := 0x01e00000
