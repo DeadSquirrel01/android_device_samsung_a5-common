82d63d81add08e48037ddf5c2517ed5c7787b925
Gaurav <allstargaurav@gmail.com>
2018-01-08 02:19:06 +0530

    Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents
    system server from loading anything from /data making our system
    extremely slow and janky.
    
    Change-Id: I4067758afd15c6fb334fde5b078e700ed1fb6f95

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,10 +212,5 @@ TW_EXCLUDE_SUPERSU      := true
 endif
 
 # Dex
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT := false
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
