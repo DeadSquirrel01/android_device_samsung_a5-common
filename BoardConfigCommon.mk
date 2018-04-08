4a760cf357d6668923783c7f4e08b62f737508d6
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-04-08 14:51:56 +0200

    Use new kernel configs of https://github.com/DeadSquirrel01/android_kernel_samsung_msm8916

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,19 +36,21 @@ TARGET_CPU_VARIANT          := cortex-a53
 TARGET_CPU_CORTEX_A53       := true
 
 # Kernel
-TARGET_KERNEL_ARCH          := arm
-BOARD_CUSTOM_BOOTIMG        := true
-BOARD_CUSTOM_BOOTIMG_MK     := hardware/samsung/mkbootimg.mk
-BOARD_DTBTOOL_ARGS          := -2
-BOARD_KERNEL_CMDLINE        := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_BASE           := 0x80000000
-BOARD_RAMDISK_OFFSET        := 0x02000000
-BOARD_KERNEL_TAGS_OFFSET    := 0x01e00000
-BOARD_KERNEL_SEPARATED_DT   := true
-BOARD_KERNEL_PAGESIZE       := 2048
-TARGET_KERNEL_SOURCE        := kernel/samsung/msm8916
-TARGET_KERNEL_CONFIG        := lineageos_a5ultexx_defconfig
-BOARD_KERNEL_IMAGE_NAME     := zImage
+TARGET_KERNEL_ARCH            := arm
+BOARD_CUSTOM_BOOTIMG          := true
+BOARD_CUSTOM_BOOTIMG_MK       := hardware/samsung/mkbootimg.mk
+BOARD_DTBTOOL_ARGS            := -2
+BOARD_KERNEL_CMDLINE          := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_BASE             := 0x80000000
+BOARD_RAMDISK_OFFSET          := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET      := 0x01e00000
+BOARD_KERNEL_SEPARATED_DT     := true
+BOARD_KERNEL_PAGESIZE         := 2048
+TARGET_KERNEL_SOURCE          := kernel/samsung/msm8916
+TARGET_KERNEL_CONFIG          := msm8916_sec_defconfig
+TARGET_KERNEL_VARIANT_CONFIG  := msm8916_sec_a5u_eur_defconfig
+TARGET_KERNEL_SELINUX_CONFIG  := selinux_defconfig
+BOARD_KERNEL_IMAGE_NAME       := zImage
 
 # Toolchain
 KERNEL_TOOLCHAIN            := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
