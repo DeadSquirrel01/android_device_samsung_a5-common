e758f8f4018c7719ade1eb59144bf38b0b30df0f
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-04-14 13:01:33 +0200

    Push some HALs to /system/vendor

--- camera/Android.mk
+++ camera/Android.mk
@@ -17,7 +17,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils \
     android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
