36c9809db201c0801fade49bb2142e861ba628ae
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-05-06 19:33:23 +0200

    Use hybris defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
-TARGET_KERNEL_CONFIG := halium_a5ultexx_defconfig
+TARGET_KERNEL_CONFIG := hybris_a5ultexx_defconfig
 #TARGET_KERNEL_CONFIG := ubuntu_a5ultexx_defconfig
 
 # Toolchain
