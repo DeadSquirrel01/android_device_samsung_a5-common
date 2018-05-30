e9f9283ef20bb78218e5e438e7100863cbc443e5
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-05-30 16:33:15 +0200

    Revert "implement zram"
    This reverts commit e6ef823ad13cb204f0d83c0d71ade767e29b3a1d.
    Woth zram, device doesn't shutdown/reboot

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -90,6 +90,14 @@ else
         echo "15360,19200,23040,26880,34415,43737" > /sys/module/lowmemorykiller/parameters/minfree
         echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
     fi
+
+    # Zram disk - 512MB size
+    zram_enable=`getprop ro.config.zram`
+    if [ "$zram_enable" == "true" ]; then
+        echo 536870912 > /sys/block/zram0/disksize
+        mkswap /dev/block/zram0
+        swapon /dev/block/zram0 -p 32758
+    fi
 fi
 }
 
