7dba8107942ad7bdcce072e16564ce11eaa4f08e
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-06-05 22:08:59 +0200

    Binderize thermal and graphics allocator

--- common.mk
+++ common.mk
@@ -130,7 +130,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
