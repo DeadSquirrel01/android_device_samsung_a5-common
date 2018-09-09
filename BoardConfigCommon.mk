345ef910759a905d2170e8023eb68de138d38371
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-09-09 21:41:45 +0000

    Allow additional gralloc 1.0 buffer usage bits
     * Our platform requires 0x02000000 for video playback.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,15 +159,16 @@ TARGET_USES_MEDIA_EXTENSIONS  := true
 TARGET_QCOM_MEDIA_VARIANT     := caf
 
 # Display
-TARGET_CONTINUOUS_SPLASH_ENABLED       := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS        := 3
-MAX_EGL_CACHE_KEY_SIZE                 := 12*1024
-MAX_EGL_CACHE_SIZE                     := 2048*1024
-OVERRIDE_RS_DRIVER                     := libRSDriver_adreno.so
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS  := true
-TARGET_USES_GRALLOC1                   := true
-TARGET_USES_HWC2                       := true
-TARGET_USES_NEW_ION_API                :=true
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS  := 0x02000000
+TARGET_CONTINUOUS_SPLASH_ENABLED         := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS          := 3
+MAX_EGL_CACHE_KEY_SIZE                   := 12*1024
+MAX_EGL_CACHE_SIZE                       := 2048*1024
+OVERRIDE_RS_DRIVER                       := libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS    := true
+TARGET_USES_GRALLOC1                     := true
+TARGET_USES_HWC2                         := true
+TARGET_USES_NEW_ION_API                  := true
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
