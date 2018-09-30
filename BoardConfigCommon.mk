2cfcca1515af2a6702b80a13a4c0d3573989bdaf
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-09-30 18:51:42 +0200

    Remove HWC2 flag
    Now HWC2 is built by default, so there is no need to keep that flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,6 @@ MAX_EGL_CACHE_SIZE                       := 2048*1024
 OVERRIDE_RS_DRIVER                       := libRSDriver_adreno.so
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS    := true
 TARGET_USES_GRALLOC1                     := true
-TARGET_USES_HWC2                         := true
 TARGET_USES_NEW_ION_API                  := true
 
 # ANT+
