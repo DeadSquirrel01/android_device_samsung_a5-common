238bdf55ec9e144aac8699399a832646b0970d74
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-10-14 08:30:04 +0200

    Remove phone-xxhdpi-2048-hwui-memory.mk include

--- common.mk
+++ common.mk
@@ -402,4 +402,3 @@ $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 
 # Dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
