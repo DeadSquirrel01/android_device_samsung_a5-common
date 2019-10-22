afb6b129d9803703832a0af9a8f97d98fab92903
DeadSquirrel01 <m.marcaccini01@gmail.com>
2019-10-22 15:37:19 +0200

    Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.

--- common.mk
+++ common.mk
@@ -314,8 +314,7 @@ PRODUCT_COPY_FILES += \
 
 # Media seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Camera
 PRODUCT_COPY_FILES += \
